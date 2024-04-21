cc32af89ec11b1b45df1c7196103b54b65146ebd
jawahar <jawaharrl2003@gmail.com>
2024-04-21 10:30:13 +0530

    path changed for login auth. and toastify corrected

--- frontend/src/Pages/Loginpage/Loginpage.jsx
+++ frontend/src/Pages/Loginpage/Loginpage.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
-import './Loginpage.css'
-// import Illustration from '../../Assets/Illustration.jpg'
-// import eye from '../../Assets/eyewhite.svg'
-import { Loginbutton } from '../../Components'
+import React, { useState } from 'react';
+import axios from "axios";
+import './Loginpage.css';
+import { useNavigate } from 'react-router-dom'
+import { Loginbutton } from '../../Components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Loginpage() {
+  const [emailid, setemailid] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate =useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post("http://localhost:8080/api/authentication/authenticate", { emailid, password })
+      .then(async(res) => {
+        console.log(res);
+        if (res.data === "Authentication successful") {
+          toast("Login Successful");
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          navigate('/personal-form')
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        toast("Invalid Emailid or Password");
+      });
+  };
+
   return (
-    <div >
+    <div>
       <div className="logincontainer">    
         <div className="Loginform">
-            <div className="form">
+          <div className="form">
             <h1 id='login-title'>Login</h1>
-            <label className='login-mailid' htmlFor="Email ID">Email ID</label>
-            <input type="text" id='input-mail' />
-            <label className='login-password' htmlFor="Password">Password</label>
-            <input  type="password" />
-            <a href="#" id='forgotpassword'> <p className='forgotpassword'>Forgot password?</p></a>
-            <div className='login-button-space' >
-            <Loginbutton></Loginbutton>
-            </div>
+            <form id="login" onSubmit={handleSubmit}>
+              <label className='login-mailid' htmlFor="Email ID">Email ID</label>
+              <input type="text" id='input-mail' onChange={e => setemailid(e.target.value)} />
+              <label className='login-password' htmlFor="Password">Password</label>
+              <input type="password" onChange={e => setPassword(e.target.value)} />
+              <a href="#" id='forgotpassword'> <p className='forgotpassword'>Forgot password?</p></a>
+              <div className='login-button-space'>
+                <Loginbutton></Loginbutton>
+               
+              </div>
+            </form>
+           
             <p className='or'>or</p>
             <p className='or new-register'>Create new account</p>
-            </div>
+          </div>
         </div>
       </div>
-      </div>
-
-   
-  )
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default Loginpage
+export default Loginpage;
